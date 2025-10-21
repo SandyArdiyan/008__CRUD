778c2b418ed190c0c9308a744d91486fd09f46c1
SandyArdiyan <sanardiyan881@gmail.com>
2025-10-21 10:12:33 +0700

    Menambahkan Database ke vscode

---
+++ index.js
@@ -0,0 +1,50 @@
+const e = require('express');
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Ikan12!',
+  database: 'biodata',
+  port: 3307
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+    return;
+  }
+    console.log('Connected Succuessfully"!');
+});
+
+app.get('/mahasiswa', (req, res) => {
+    db.query('SELECT * FROM mahasiswa', (err, results) => {
+        if (err) {
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        res.json(results);
+    
+        res.json(results);
+    });
+});
+
+
